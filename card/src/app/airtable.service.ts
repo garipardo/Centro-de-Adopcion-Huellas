8ab82605d463e9e28fc2adea21a98b86a0641555
Jack625a <77822933+Jack625a@users.noreply.github.com>
2024-09-27 10:42:43 -0400

    Conexion Base de Datos

---
+++ card/src/app/airtable.service.ts
@@ -0,0 +1,60 @@
+import { Injectable } from '@angular/core';
+//Paso 1. Importar Airtable
+import Airtable from 'airtable';
+//Paso 2. Importar las variables de entorno
+import { environment } from '../environments/environment';
+//Paso 3. Importar la conexion con el servicio y angular
+import { Observable, from } from 'rxjs';
+
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AirtableService {
+  //Paso 4. Crear la varaibel de conexion
+  private base:any;
+  constructor() { 
+    //Paso 5. Configurar la conexion con Airtable para token y el idBase
+    Airtable.configure({
+      endpointUrl:'https://api.airtable.com',
+      apiKey:environment.airtableToken
+    });
+    //Paso 6. Configurar el id de la base de datos
+    this.base=Airtable.base(environment.airtableIdBase);
+  }
+  //Paso 7. Crear el metodo para obtener los registros de la tabla Mascotas
+  obtenerMascotas():
+  Observable<any[]>{
+    return from(new Promise<any[]>((resolve,reject)=>{
+      const registros:any[]=[];
+      this.base('Mascotas').select({
+        view:'Grid View'
+      }).eachPage(
+        function page(records: any[],siguiente: () => void){
+          //Agregar los registros obtenidos a nuestra lista
+          records.forEach(registro=>{
+            registros.push({
+              id:registro.id,
+              nombre:registro.get('Nombre'),
+              edad:registro.get('Edad'),
+              sexo:registro.get('Sexo'),
+              contacto:registro.get('Contacto'),
+              foto:registro.get('Foto'),
+              tipoMascota:registro.get('TipoMascota')
+            });
+          });
+          siguiente();
+
+        },
+        function salir(error: any){
+          if(error){
+            reject(error);
+          }else{
+            resolve(registros);
+          }
+        }
+      );
+    }));
+  }
+}
